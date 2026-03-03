4fb61379ac1586faa33c847f6318ac8cf6e3242b
soaphaa <sophia.a.luo@gmail.com>
2026-03-03 11:18:08 -0500

    timer window BIG BALLLL

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //main js file!
+console.log("script loaded");
+const toggleButton = document.getElementById("toggle-start-btn");
+let isRunning = false;
+
+function startTimer(){
+ console.log("Timer started");
+ toggleButton.textContent = 'Stop';
+ isRunning = true; 
+ toggleButton.classList.remove('start');
+ toggleButton.classList.add('stop');
+}
+
+function stopTimer(){
+    console.log("Timer stopped");
+    isRunning = false;
+    toggleButton.textContent = 'Start';
+    toggleButton.classList.remove('stop');
+    toggleButton.classList.add('start');
+}
+
+
+toggleButton.addEventListener("click", function() {
+    if (isRunning === false) {
+        startTimer();
+    } else {
+        stopTimer();
+    }
+});
