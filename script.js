e25fb37ea6d77cf23bee019601e83ff263a1958a
Alyn <alynte3@gmail.com>
2026-03-03 14:10:00 -0500

    got the recipes working lets go
    got the respective recipes working it js lowk looks a lil ahh !!

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 const btn = document.getElementById("submit");
-
+const recipes = [{
+    title: "Cookeroonies",
+    description: "yum cooks",
+    tags: ["sweet", "dessert"]
+},
+{
+    title: "alyn salmon", description: "spicy ahh salmon", tags: ["savory", "spicy"]
+}]
+const reccontainer = document.getElementById("recipe-container")
+const tagbtns = document.getElementById("tagbtns")
 btn.addEventListener("click", function () {
-  document.getElementById("spoof").innerHTML = "ts pmo oh em gee";
+  document.getElementById("spoof").innerHTML = "free me";
 });
+function displayRec(filteredRecipes){
+    reccontainer.innerHTML = "";
+    filteredRecipes.forEach(recipe =>{
+        const card = document.createElement("body");
+        card.classList.add("recipe-card");
+
+        const title = document.createElement("h2");
+        title.textContent = recipe.title;
+
+        const description = document.createElement("p");
+        description.textContent = recipe.description;
+
+        card.appendChild(title);
+        card.appendChild(description);
+        
+        reccontainer.appendChild(card);
+    })
+}
+
+displayRec(recipes);
