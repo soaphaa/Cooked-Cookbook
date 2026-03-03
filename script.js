80dee328b54e1b7ed38e37daac375da74752884c
Alyn <alynte3@gmail.com>
2026-03-03 15:53:56 -0500

    buttons + recipes
    added more recipe flair/design (salmon and index html exclusive)
    fully functional "recipe checking buttons"
    fully functional homepage button !!
    ignore the crumbling corpses logo i was js testing smth

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const btn = document.getElementById("submit");
 const recipes = [{
     title: "Cookeroonies",
     description: "yum cooks",
-    tags: ["sweet", "dessert"]
+    tags: ["sweet", "dessert"], source: "cookies.html"
 },
 {
-    title: "alyn salmon", description: "spicy ahh salmon", tags: ["savory", "spicy"]
+    title: "alyn salmon", description: "spicy ahh salmon",
+    tags: ["savory", "spicy"], source: "salmon.html"
 }]
 const reccontainer = document.getElementById("recipe-container")
 const tagbtns = document.getElementById("tagbtns")
@@ -21,10 +22,16 @@ function displayRec(filteredRecipes){
         const title = document.createElement("h2");
         title.textContent = recipe.title;
 
-        const description = document.createElement("p");
+        const description = document.createElement("rbody");
         description.textContent = recipe.description;
 
+        const f_btn = document.createElement("a");
+        f_btn.textContent = "View Recipe trust";
+        f_btn.href = recipe.source;
+        f_btn.classList.add("recipe-btn")
+
         card.appendChild(title);
+        card.appendChild(f_btn);
         card.appendChild(description);
         
         reccontainer.appendChild(card);
