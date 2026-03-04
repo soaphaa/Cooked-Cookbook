a85e850007d2e08bea0099091b4a9012371f3b34
soaphaa <sophia.a.luo@gmail.com>
2026-03-04 08:59:41 -0500

    combined my timer with alyn's recipe pages!! Lowk css isnt working

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 //main js file!
 const timerToggleBtn = document.getElementById("timer-toggle-btn");
-const timerWindow = document.getElementById("timer");
-const timerMinimizeBtn = document.getElementById("timer-minimize-btn");
 let timerPopup = null; //to track the popup window state
 
 timerToggleBtn.addEventListener("click", function() {
     if(timerPopup && !timerPopup.closed){
         timerPopup.focus(); //bring existing popup to front
     } else {
-        timerPopup = window.open("timer.html", "Timer", "width=300,height=200"); //open new popup
-    }
-    // timerWindow.classList.toggle("hidden");
-});
-
-timerMinimizeBtn.addEventListener("click", function() {
-    timerWindow.classList.add("hidden");
-});
+        timerPopup = window.open(
+            "timer.html",
+            "Timer",
+            "width=300,height=200, top=100,right=100,",
+            "resizable=no"); //open new popup
+        }
+    });
+    
