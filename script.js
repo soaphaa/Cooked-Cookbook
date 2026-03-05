74b4cc7b4a11a686d8f4494ff16d8448fc020e99
Claire <czhu7@ocdsb.ca>
2026-03-05 11:04:44 -0500

    Added pics for each recipe

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const u_search = document.getElementById("user_search")
 const recipes = [{
     title: "Cookeroonies",
     description: "yum cooks",
-    tags: ["sweet", "dessert"], source: "cookies.html"
+    tags: ["sweet", "dessert"], 
+    source: "cookies.html",
+    image: "images/chocolate-chip-cookie.jpeg"
 },
 {
     title: "alyn salmon", description: "spicy ahh salmon",
-    tags: ["savory", "spicy"], source: "salmon.html"
+    tags: ["savory", "spicy"], source: "salmon.html",
+    image: "images/salmon.jpg"
 }]
 const reccontainer = document.getElementById("recipe-container");
 const tagbtns = document.getElementById("tagbtns");
@@ -27,11 +30,17 @@ function displayRec(filteredRecipes){
         const description = document.createElement("rbody");
         description.textContent = recipe.description;
 
+        const image = document.createElement("img");
+        image.src = recipe.image;
+        image.alt = recipe.title + " Image";
+        image.classList.add("recipe-image");
+
         const f_btn = document.createElement("a");
         f_btn.textContent = "View Recipe trust";
         f_btn.href = recipe.source;
         f_btn.classList.add("recipe-btn")
 
+        card.appendChild(image);
         card.appendChild(title);
         card.appendChild(f_btn);
         card.appendChild(description);
