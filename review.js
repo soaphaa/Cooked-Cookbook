e376054bb666dcebdfde9d82d1b826808c966e7e
Claire <czhu7@ocdsb.ca>
2026-03-16 12:41:48 -0400

    commit staged

--- review.js
+++ review.js
@@ -22,7 +22,9 @@ const reviewSection = document.querySelector(".review-section");
 
 const reviewTemplate = document.querySelector("#review-template");
 const ratingsTemplate = document.querySelector("#ratings-template");
+const ratingButtonTemplate = document.querySelector("#rating-button-template");
 const ratingBarTemplate = document.querySelector("#rating-bar-template");
+const uploadedImgTemplate = document.querySelector("#uploaded-img-template");
 const reviewImgTemplate = reviewTemplate.content.querySelector("#img-template");
 
 
@@ -37,6 +39,146 @@ reviews.forEach(review => {
 
 updateReviewSummary();
 
+initializeReviewWriting();
+
+function initializeReviewWriting() {
+    const writeReviewButton = document.querySelector(".write-review-btn");
+    const reviewPopupOverlay = document.querySelector(".overall-review-container").querySelector(".popup-overlay");
+    const reviewPopup = document.querySelector(".review-popup");
+    writeReviewButton.addEventListener("click", () => {
+        reviewPopupOverlay.classList.add("show");
+        reviewPopup.classList.add("show");
+    })
+
+    const userRatings = document.querySelector(".user-ratings");
+    for (let i = 0; i < 5; i++) {
+        const ratingBTNClone = ratingButtonTemplate.content.cloneNode(true);
+        const ratingBTN = ratingBTNClone.querySelector("button");
+        ratingBTN.querySelector("i").classList.add("far");
+
+        ratingBTN.addEventListener("click", () => {
+            const ratingButtons = userRatings.querySelectorAll(".rating-button");
+            let isTargetBTNReached = false;
+            for (let i = 0; i < 5; i++) {
+                const buttonIcon = ratingButtons[i].querySelector("i");
+                if (isTargetBTNReached) {
+                    buttonIcon.classList.add("far");
+                    buttonIcon.classList.remove("fas");
+                } else {
+                    buttonIcon.classList.remove("far");
+                    buttonIcon.classList.add("fas");
+                }
+                if (ratingBTN === ratingButtons[i]) {
+                    isTargetBTNReached = true;
+                }
+            }
+            changeSubmitBTN();
+        })
+        userRatings.appendChild(ratingBTNClone);
+    }
+
+    const reviewTextbox = document.querySelector(".review-textbox");
+    reviewTextbox.addEventListener("input", () => {
+        changeSubmitBTN();
+    })
+
+    const uploadedImages = document.querySelector(".uploaded-imgs-container");
+
+    const uploadedFiles = [];
+    const fileInput = document.querySelector("#img-upload");
+
+    fileInput.addEventListener("change", () => {
+        const file = fileInput.files[0];
+        let doesFileExist = false;
+
+        if (uploadedFiles.length > 0) {
+            for (let i = 0; i < uploadedFiles.length; i++) {
+                if (uploadedFiles[i].name === file.name && uploadedFiles[i].size === file.size && uploadedFiles[i].lastModified === file.lastModified) {
+                    doesFileExist = true;
+                    break;
+                }
+            }
+        }
+        if (!doesFileExist) {
+            uploadedFiles.push(file);
+            const uploadedImgClone = uploadedImgTemplate.content.cloneNode(true);
+            const uploadedImgDiv = uploadedImgClone.firstElementChild;
+            uploadedImgDiv.querySelector("img").src = URL.createObjectURL(file);
+
+            uploadedImgDiv.querySelector("button").addEventListener("click", () => {
+                const uploadedImgContainers = uploadedImages.querySelectorAll(".uploaded-img");
+                for (let i = 0; i < uploadedImgContainers.length; i++) {
+                    if (uploadedImgDiv === uploadedImgContainers[i]) {
+                        uploadedFiles.splice(i, 1);
+                        uploadedImages.removeChild(uploadedImgContainers[i]);
+                        break;
+                    }
+                }
+                changeUploadImagesBTN(uploadedFiles);
+                changeSubmitBTN();
+            })
+            uploadedImages.appendChild(uploadedImgDiv);
+        }
+        changeUploadImagesBTN(uploadedFiles);
+        changeSubmitBTN();
+    })
+
+    const cancelButton = document.querySelector("#cancel-btn");
+    cancelButton.addEventListener("click", () => {
+        const ratingButtons = userRatings.querySelectorAll(".rating-button");
+        ratingButtons.forEach(button => {
+            const buttonIcon = button.querySelector("i");
+            buttonIcon.classList.remove("fas");
+            buttonIcon.classList.add("far");
+        })
+
+        const textbox = reviewPopup.querySelector(".review-textbox");
+        textbox.value = "";
+
+        uploadedImages.innerHTML = "";
+
+        reviewPopup.classList.remove("show");
+        reviewPopupOverlay.classList.remove("show");
+    })
+
+    const submitButton = document.querySelector("#submit-btn");
+    changeSubmitBTN();
+    // submitButton.addEventListener
+}
+
+function changeUploadImagesBTN(imagesArray) {
+    const imageUpload = document.querySelector("#img-upload");
+    if (imagesArray.length >= 7) {
+        imageUpload.disabled = true;
+    } else {
+        imageUpload.disabled = false;
+    }
+}
+
+function changeSubmitBTN() {
+    const submitButton = document.querySelector("#submit-btn");
+
+    let isRatingsEdited = false;
+    const userRatings = document.querySelector(".user-ratings .rating-button");
+    userRatings.some(button => {
+        if (button.querySelector("i").classList.contains("fas")) {
+            isRatingsEdited = true;
+            return true;
+        }
+    })
+    if (isRatingsEdited) {
+        const textbox = document.querySelector(".review-textbox");
+        const uploadedImgsContainer = document.querySelector(".uploaded-imgs-container");
+        if (textbox.value.trim() !== "" || uploadedImgsContainer.children.length !== 0) {
+            submitButton.disabled = false;
+        } else {
+            submitButton.disabled = true;
+        }
+    } else {
+        submitButton.disabled = true;
+    }
+}
+
 function initializeReviewSummary() {
     const overallRatings = document.querySelector(".overall-ratings");
     const ratingBars = document.querySelector(".rating-bars-container");
@@ -48,7 +190,7 @@ function initializeReviewSummary() {
 
         const ratingBarClone = ratingBarTemplate.content.cloneNode(true);
         const ratingLevel = ratingBarClone.querySelector(".rating-level");
-        ratingLevel.textContent = (i+1).toFixed(1);
+        ratingLevel.textContent = (5 - i).toFixed(1);
         ratingBars.append(ratingBarClone);
     }
 }
@@ -64,77 +206,79 @@ function updateReviewSummary() {
 
     const ratingBarsContainer = document.querySelector(".rating-bars-container");
     const ratingBars = ratingBarsContainer.querySelectorAll(".rating-bar");
-    for (let i = 0; i < ratingBars.length; i++) {
-        const percentage = (ratingsDistribution[i]/reviews.length * 100).toFixed(0);
-        
+    for (let i = 0; i < 5; i++) {
+        const percentage = (ratingsDistribution[5 - i - 1] / reviews.length * 100).toFixed(0);
+
         const ratingPercentage = ratingBars[i].querySelector(".rating-percentage");
-        ratingPercentage.textContent = percentage + "% (" + getNumInTwoDigits(ratingsDistribution[i]) + ")";
+        ratingPercentage.textContent = percentage + "% (" + getNumInTwoDigits(ratingsDistribution[5 - i - 1]) + ")";
         // ratingPercentage.textContent = "100% (999K)";
 
         const ratingBarFilled = ratingBars[i].querySelector(".rating-bar-filled");
         ratingBarFilled.style.width = percentage + "%";
     }
-    
+
 }
+
+
 
 function displayReview(review) {
     const reviewClone = reviewTemplate.content.cloneNode(true);
 
-        const username = reviewClone.querySelector("#username");
-        username.textContent = review.username;
+    const username = reviewClone.querySelector("#username");
+    username.textContent = review.username;
 
-        const date = reviewClone.querySelector("#date");
-        date.textContent = review.date;
+    const date = reviewClone.querySelector("#date");
+    date.textContent = review.date;
 
-        const ratings = reviewClone.querySelector(".ratings");        
-        // ratings.innerHTML = "";
+    const ratings = reviewClone.querySelector(".ratings");
+    // ratings.innerHTML = "";
 
-        for (let i = 0; i < 5; i++) {
-            const ratingsClone = ratingsTemplate.content.cloneNode(true);
-            const star = ratingsClone.querySelector("i");
+    for (let i = 0; i < 5; i++) {
+        const ratingsClone = ratingsTemplate.content.cloneNode(true);
+        const star = ratingsClone.querySelector("i");
 
-            if (i < review.ratings) {
-                star.classList.add("fas");
-            } else {
-                star.classList.add("far");
-            }
-            ratings.appendChild(ratingsClone);
+        if (i < review.ratings) {
+            star.classList.add("fas");
+        } else {
+            star.classList.add("far");
         }
+        ratings.appendChild(ratingsClone);
+    }
 
-        totalRatings += review.ratings;
-        ratingsDistribution[review.ratings-1]++;
+    totalRatings += review.ratings;
+    ratingsDistribution[review.ratings - 1]++;
 
-        const reviewParagraph = reviewClone.querySelector("#review-paragraph");
-        reviewParagraph.textContent = review.review;
+    const reviewParagraph = reviewClone.querySelector("#review-paragraph");
+    reviewParagraph.textContent = review.review;
 
-        const reviewImages = reviewClone.querySelector(".review-images");
-        
-        review.pictures.forEach(reviewPicture => {
-            const reviewImgClone = reviewImgTemplate.content.cloneNode(true);
-            const image = reviewImgClone.querySelector("img");
-            const popUpImage = reviewImgClone.querySelector(".popup-image img");
-            image.src = reviewPicture;
-            popUpImage.src = reviewPicture;
+    const reviewImages = reviewClone.querySelector(".review-images");
 
-            const imageButton = reviewImgClone.querySelector(".review-image-btn");
-            const popupOverlay = reviewImgClone.querySelector(".popup-overlay");
-            const popup = reviewImgClone.querySelector(".img-popup");
-            const closePopupButton = reviewImgClone.querySelector(".close-img-btn");
+    review.pictures.forEach(reviewPicture => {
+        const reviewImgClone = reviewImgTemplate.content.cloneNode(true);
+        const image = reviewImgClone.querySelector("img");
+        const popUpImage = reviewImgClone.querySelector(".popup-image img");
+        image.src = reviewPicture;
+        popUpImage.src = reviewPicture;
 
-            imageButton.addEventListener("click", () => {
-                popupOverlay.classList.add("show");
-                popup.classList.add("show");
-            });
+        const imageButton = reviewImgClone.querySelector(".review-image-btn");
+        const popupOverlay = reviewImgClone.querySelector(".popup-overlay");
+        const popup = reviewImgClone.querySelector(".img-popup");
+        const closePopupButton = reviewImgClone.querySelector(".close-img-btn");
 
-            closePopupButton.addEventListener("click", () => {
-                popupOverlay.classList.remove("show");
-                popup.classList.remove("show");
-            })
+        imageButton.addEventListener("click", () => {
+            popupOverlay.classList.add("show");
+            popup.classList.add("show");
+        });
 
-            reviewImages.appendChild(reviewImgClone);
+        closePopupButton.addEventListener("click", () => {
+            popupOverlay.classList.remove("show");
+            popup.classList.remove("show");
         })
-        
-        reviewSection.appendChild(reviewClone);
+
+        reviewImages.appendChild(reviewImgClone);
+    })
+
+    reviewSection.appendChild(reviewClone);
 }
 
 
@@ -144,10 +288,10 @@ function getNumInTwoDigits(number) {
     } else if (number < 10000) {
         return (number / 1000).toFixed(1) + "K"; //9000
     } else if (number < 1000000) {
-        return Math.round(number/1000) + "K";
+        return Math.round(number / 1000) + "K";
     } else if (number < 10000000) {
         return (number / 1000000).toFixed(1) + "M";
     } else if (number < 1000000000) {
-        return Math.round(number/1000000) + "M";
+        return Math.round(number / 1000000) + "M";
     }
 }
--- script.js
+++ script.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const home = document.getElementById("home-button");
 
-if (home){
+if (home) {
     home.innerHTML = "";
     const hbt = document.createElement("a");
     hbt.textContent = "HOMEPAGE"
@@ -71,7 +71,7 @@ const u_search = document.getElementById("user_search")
 const recipes = [{
     title: "Cookeroonies",
     description: "yum cooks",
-    tags: ["sweet", "dessert"], 
+    tags: ["sweet", "dessert"],
     ingredients: ["flour", "sugar", "eggs", "chocolate chips"],
     source: "cookies.html",
     title: "Chocolate Chip Cookies",
@@ -83,13 +83,28 @@ const recipes = [{
     title: "Alyn Salmon",
     description: "alyn salmon: gochujang, mirin, soy sauce, salmon, sesame oil, sugar",
     tags: ["savory", "spicy", "sweet"], source: "salmon.html",
-    image: "images/salmon.jpg"
+    image: "images/alyn's salmon.jpeg"
 },
 {
     title: "Chocolate Chip Banana Bread",
     description: "chocolate chip banana bread: chocolate chips, banana, flour, egg, milk",
     tags: ["sweet", "dessert"],
-    source: "banana bread.html"
+    source: "banana bread.html",
+    image: "images/chocolate-chip-cookie.jpeg"
+},
+{
+    title: "Kimchi Fried Rice",
+    description: "Kimchi Fried Rice: kimchi, rice, sesame oil, gochujang, soy sauce, garlic, green onions, egg",
+    tags: ["savory", "spicy"],
+    source: "fried rice.html",
+    image: "images/fried-rice.png"
+},
+{
+    title: "Salad",
+    description: "Arugula Salad: arugula, cherry tomatoes, cucumber, feta cheese, olive oil, balsamic vinegar",
+    tags: ["savory", "healthy"],
+    source: "salad.html",
+    image: "images/salad.png"
 },
 {
     title: "Kimchi Fried Rice",
@@ -122,21 +137,21 @@ const filterset = document.getElementById("tagbtns")
 btn.addEventListener("click", function () {
     const textValue = u_search.value.trim();
     search(textValue);
-//take value from the search bar
+    //take value from the search bar
 });
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     const textValue = u_search.value.trim();
     if (event.key === 'Enter' && textValue != null) {
         search(textValue);
     }
 });
 
-function search(txt){
-    if (!reccontainer) return;  
+function search(txt) {
+    if (!reccontainer) return;
     reccontainer.innerHTML = "";
-    recipes.forEach(recipe =>{
-        if ((recipe.description.toLowerCase().includes(txt.toLowerCase()))){
+    recipes.forEach(recipe => {
+        if ((recipe.description.toLowerCase().includes(txt.toLowerCase()))) {
             const card = document.createElement("div");
             const title = document.createElement("rh1");
             const description = document.createElement("rbody");
@@ -170,25 +185,25 @@ function search(txt){
             card.appendChild(image);
             card.appendChild(f_btn);
             card.appendChild(tagcont);
-            
+
             reccontainer.appendChild(card);
         }
     })
-    if (reccontainer.innerHTML == ""){
-            const no_card = document.createElement("p");
-            no_card.classList.add("warning");
-            no_card.textContent = "sorry !! there's no recipes available with that ingredient/title !!";
-            reccontainer.appendChild(no_card);
+    if (reccontainer.innerHTML == "") {
+        const no_card = document.createElement("p");
+        no_card.classList.add("warning");
+        no_card.textContent = "sorry !! there's no recipes available with that ingredient/title !!";
+        reccontainer.appendChild(no_card);
     }
 }
 
-function genTagBtns(){
+function genTagBtns() {
     //new list?? ish of tags
     const allTags = new Set();
 
     //for each recipes, for each tag of recipe, add to the all tags set
-    recipes.forEach(recipe =>{
-        recipe.tags.forEach(tag=>{
+    recipes.forEach(recipe => {
+        recipe.tags.forEach(tag => {
             allTags.add(tag);
         })
     })
@@ -199,13 +214,13 @@ function genTagBtns(){
 
 
     //create tags for the different ones
-    allTags.forEach(tag =>{
+    allTags.forEach(tag => {
         createTagBtn(tag);
     })
 }
 
 
-function createTagBtn(tagname){
+function createTagBtn(tagname) {
     const tagbtns = document.createElement("div");
     tagbtns.classList.add("filterlist");
     const tbtn = document.createElement("button");
@@ -213,10 +228,10 @@ function createTagBtn(tagname){
     tbtn.textContent = tagname;
     tbtn.classList.add("recipe-filter");
     tbtn.addEventListener("click", () => {
-        if (tagname == "All"){
+        if (tagname == "All") {
             //if the filter is js all of them
             displayRec(recipes);
-        }else{
+        } else {
             //filtered recipes = recipes that have "tagname" (the name of the filtered tag)
             const filtrecipes = recipes.filter(recipe =>
                 recipe.tags.includes(tagname));
@@ -224,15 +239,15 @@ function createTagBtn(tagname){
             //display the filtered set
         }
     });
-    
+
     filterset.appendChild(tbtn);
 
 }
- 
-function displayRec(filteredRecipes){
+
+function displayRec(filteredRecipes) {
     if (!reccontainer) return;
     reccontainer.innerHTML = "";
-    filteredRecipes.forEach(recipe =>{
+    filteredRecipes.forEach(recipe => {
         const card = document.createElement("div");
         const title = document.createElement("rh1");
         const description = document.createElement("rbody");
@@ -257,19 +272,19 @@ function displayRec(filteredRecipes){
         tagcont.classList.add("taglist");
 
         recipe.tags.forEach(tag => {
-             const rtag = document.createElement("rbody");
-             rtag.classList.add("recipe-tag");
-             rtag.textContent = tag;
-             tagcont.appendChild(rtag);
+            const rtag = document.createElement("rbody");
+            rtag.classList.add("recipe-tag");
+            rtag.textContent = tag;
+            tagcont.appendChild(rtag);
         });
 
         card.appendChild(title);
         card.appendChild(image);
         card.appendChild(f_btn);
         card.appendChild(tagcont)
-        
+
         reccontainer.appendChild(card);
-     })
+    })
 }
 
 genTagBtns();
@@ -281,7 +296,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("timer-toggle-btn not found on this page");
         return;
     }
-    
+
     let timerPopup = null;
     timerToggleBtn.addEventListener("click", () => {
         console.log("Timer toggle button clicked");
@@ -293,13 +308,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     const confettiBtn = document.getElementById("finish");
-        confettiBtn.addEventListener("click", () => {
-            confetti({
-                particleCount: 2000,
-                spread: 6000,
-                origin: { y: 0.6 }
-            })
-        });
+    confettiBtn.addEventListener("click", () => {
+        confetti({
+            particleCount: 2000,
+            spread: 6000,
+            origin: { y: 0.6 }
+        })
+    });
 
 
     checkboxes.forEach(cb => {
@@ -316,3 +331,25 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     
 });
+
+// function displayReviewSection() {
+//     const reviewSection = document.createElement("div");
+//     fetch("review.html")
+//         .then(response => response.text())
+//         .then(html => {
+//             reviewSection.innerHTML = html;
+//             document.body.appendChild(reviewSection);
+
+//             const reviewJS = document.createElement("reviewScript");
+//             reviewJS.src = "review.js";
+//             document.body.appendChild(reviewJS);
+//         });
+// }
+
+// recipes.tags.forEach(recipeHTML => {
+//     const filePath = window.location.pathname.split("/").pop();
+
+//     if (filePath == recipeHTML) {
+//         displayReviewSection();
+//     }
+// });
